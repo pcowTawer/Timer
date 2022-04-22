2f4b4cb3628ab4ea90b0b79eee88d874f19f7898
Ilya_Prostakov <prostocowtawer@gmail.com>
2022-04-22 13:15:06 +0700

    Add count of hours

--- timer.c
+++ timer.c
@@ -39,8 +39,8 @@ int main(int argc, char *argv[])
     char* command_write = (char*)malloc(sizeof(char) * 255);
     char* command_add =   (char*)malloc(sizeof(char) * 255);
     snprintf(command_start, sizeof(char) * 255, "%s%s", "start /w ", argv[1]);
-    snprintf(command_write, sizeof(char) * 255, "%s%s", "echo %time:~3,8% > ", txt_file_name);
-    snprintf(command_add, sizeof(char) * 255, "%s%s", "echo %time:~3,8% >> ", txt_file_name);
+    snprintf(command_write, sizeof(char) * 255, "%s%s", "echo %time% > ", txt_file_name);
+    snprintf(command_add, sizeof(char) * 255, "%s%s", "echo %time% >> ", txt_file_name);
 
     system(command_write);
     system(command_start);
@@ -52,9 +52,17 @@ int main(int argc, char *argv[])
 
     f = fopen(txt_file_name, "r+t");
     char buf;
+    int hour1 = 0, hour2 = 0;
     int min1 = 0, min2 = 0;
     double sec1 = 0, sec2 = 0;
 
+
+    fscanf(f, "%c", &buf);
+    hour1 += 10 * ((int)buf - 48);
+    fscanf(f, "%c", &buf);
+    hour1 += (int)buf - 48;
+    fscanf(f, "%c", &buf);
+    
     fscanf(f, "%c", &buf);
     min1 += 10 * ((int)buf - 48);
     fscanf(f, "%c", &buf);
@@ -73,6 +81,11 @@ int main(int argc, char *argv[])
     fscanf(f, "%c", &buf);
     fscanf(f, "%c", &buf);
 
+    fscanf(f, "%c", &buf);
+    hour2 += 10 * ((int)buf - 48);
+    fscanf(f, "%c", &buf);
+    hour2 += (int)buf - 48;
+    fscanf(f, "%c", &buf);
 
     fscanf(f, "%c", &buf);
     min2 += 10 * ((int)buf - 48);
@@ -92,7 +105,7 @@ int main(int argc, char *argv[])
     fscanf(f, "%c", &buf);
     fseek(f, 0, SEEK_END);
 
-    double result = min2*60 - min1*60 + sec2 - sec1;
+    double result = hour2*3600 - hour1*3600 + min2*60 - min1*60 + sec2 - sec1;
     char* result_print;
     fprintf(f, "%.2f", result);
 
